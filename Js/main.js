f0095a07ebfaa91be853768192d801b246e66fb4
Karl Smith <98028208+Ksmithprofessional@users.noreply.github.com>
2022-05-28 11:43:21 +0100

    Added email validation

--- Js/main.js
+++ Js/main.js
@@ -38,3 +38,36 @@ imgChange.addEventListener('click', async () => {
         })
     });
 });
+
+
+//email validation
+
+const formSubmit = document.querySelector('#submit');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+
+    let email = document.querySelector('#email input').value;
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(!email.match(regex)) {
+
+        e.preventDefault();
+        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> Invalid email address`;
+        // console.log(email);
+        // test to see whether the email address actually shows up on failure
+
+    }
+});
+
+form.addEventListener('submit', (e) => {
+
+    let email = document.querySelector('#email input').value;
+
+    if(email === '') {
+
+        e.preventDefault();
+        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> We can't email you if you don't put in an email address!`;
+
+    }
+});
