26197a3494e18a9de50c396201df3dbe4b4f9d22
Karl Smith <98028208+Ksmithprofessional@users.noreply.github.com>
2022-06-02 17:52:30 +0100

    Added more layout functionality and styling

--- Js/main.js
+++ Js/main.js
@@ -21,7 +21,7 @@ fetch(url)
 imgChange.addEventListener('click', async () => {
 
     img.src = ''; 
-    author.innerHTML = '<p>Photographer: </p>'
+    author.innerHTML = 'Photographer: '
 
     let infoLink = new Promise((resolve, reject) => {
         fetch(url)
@@ -117,3 +117,33 @@ form.addEventListener('submit', (e) => {
             // console.log(matchArray.every(allFalse))
     } 
 });
+
+// clock (granted i just looked this one up and edited it a little)
+
+function showTime(){
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    let time = h + ":" + m  + " " + session;
+    document.querySelector(".time").innerText = time;
+    document.querySelector(".time").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
